5b0439d6c50f16ac8b9c0d080f243e35d22dd56b
Riley Barabash <admin@rileybarabash.com>
2025-01-11 07:22:24 -0700

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,13 +7,15 @@ import { NextResponse, type NextRequest } from "next/server"
 export async function middleware(request: NextRequest): Promise<NextResponse> {
     const { hostname, pathname, search, hash } = request.nextUrl
 
+    if (pathname.startsWith("/_next") || pathname.startsWith("/api") || pathname.includes(".")) return NextResponse.next()
+
     if (hostname.endsWith("altered.app") && pathname.startsWith("/kaitype"))
         return NextResponse.redirect(
             new URL(`https://kaitype.co${pathname.replace("/kaitype", "")}${search}${hash}`, request.url),
             { status: 308 }
         )
 
-    if (hostname.endsWith("kaitype.co") && !pathname.startsWith("/_next") && !pathname.startsWith("/api"))
+    if (hostname.endsWith("kaitype.co"))
         return NextResponse.rewrite(new URL(`/kaitype${pathname}${search}${hash}`, request.url))
 
     return NextResponse.next()
