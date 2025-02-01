133b1b84a6216ff6e7794051973cff657892e27b
Riley Barabash <admin@rileybarabash.com>
2025-02-01 04:50:02 -0700

    build: route to edge

--- src/app/api/raycast/echo/route.ts
+++ src/app/api/raycast/echo/route.ts
@@ -5,6 +5,8 @@
 import { NextResponse, type NextRequest } from "next/server"
 import { getDeploymentInfo } from "./get-deployment-info"
 
+export const runtime = "edge"
+
 export async function GET(_req: NextRequest): Promise<NextResponse> {
     return NextResponse.json(await getDeploymentInfo())
 }
